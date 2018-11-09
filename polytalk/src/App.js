7fbd20eb675ec6f79270101af611cbb154addc36
Allina <allina606@gmail.com>
2018-11-08 23:39:37 -0500

    code

--- polytalk/src/App.js
+++ polytalk/src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      officials: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://www.googleapis.com/civicinfo/v2/representatives?address=10304&key=YOURAPIKEYHERE")
+    .then(results => {
+      return results.json();
+    }).then (data => {
+        let officials = data.results.map((ofName) => {
+          return (
+            <div key={ofName.results}>
+              <p>{ofName.officials.name}</p>
+            </div>
+          )
+        })
+    this.setState({officials: officials});
+    console.log("state", this.state.officials); 
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>PoliTalk: Search Representatives</h1> 
+
+        <form ref="representatives">
+          <input type = "text" ref="zip_code" placeholder="zip code"></input>
+         {/* <button onclick = {this.getRep.bind(this)}>Search</button>  */}
+        </form>
+
+        {this.state.officials}
       </div>
     );
   }
