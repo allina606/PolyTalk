e2f66f41237dd9bb10bffd0870a580759823ac3d
Nouraiz <nouraiz_imran@hotmail.com>
2018-11-09 20:20:24 -0500

    Fixed the errors that were there and we now seccesfully get the json object from the api also made it dynamic on the front end.

--- polytalk/src/App.js
+++ polytalk/src/App.js
@@ -1,46 +1,73 @@
 import React, { Component } from 'react';
 import './App.css';
+import { GOOGLE_KEY } from './keys.js';
+
+function ZipSearchField(props){
+  return(
+    <div> 
+    <label>ZipCode:</label>
+    <input onChange={props.changeHandler} type="text"  placeholder="10304" className="text-center"/>
+    </div>
+  )}
+
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      officials: [],
-    };
+      officials : []
+    }
   }
 
-  componentDidMount() {
-    fetch("https://www.googleapis.com/civicinfo/v2/representatives?address=10304&key=YOURAPIKEYHERE")
-    .then(results => {
-      return results.json();
-    }).then (data => {
-        let officials = data.results.map((ofName) => {
-          return (
-            <div key={ofName.results}>
-              <p>{ofName.officials.name}</p>
-            </div>
-          )
-        })
-    this.setState({officials: officials});
-    console.log("state", this.state.officials); 
+
+  zipCodeChanged(event) {
+    if(event.target.value.length !== 5){
+      this.setState({officials: []});
+      return(
+        <div>
+        <p>No Result</p>
+        </div>
+      )
+    }
+    const url = "https://www.googleapis.com/civicinfo/v2/representatives?address="+ event.target.value +"&key=" + GOOGLE_KEY
+
+    fetch(url,{
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+          "Content-Type": "application/json; charset=utf-8",
+          // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer" // no-referrer, *client
     })
+    .then( (response) => {
+      if(response.status === 200)
+        return(response.json())
+      else
+        console.log("There was an error")
+    })
+    .then( (body) => {
+      console.log(body)
+      this.setState({officials: body})
+      console.log(this.officials)
+
+
+    })
+    
   }
 
   render() {
     return (
-      <div className="App">
-        <h1>PoliTalk: Search Representatives</h1> 
-
-        <form ref="representatives">
-          <input type = "text" ref="zip_code" placeholder="zip code"></input>
-         {/* <button onclick = {this.getRep.bind(this)}>Search</button>  */}
-        </form>
-
-        {this.state.officials}
+      <div className = "App"> 
+      <h1>PoliTalk: Search Representatives</h1> 
+      <ZipSearchField changeHandler={(e) => this.zipCodeChanged(e)}/>
       </div>
-    );
-  }
+      
+    ); 
+  };
 }
+
 
 export default App;
---
+++ polytalk/src/keys.js
@@ -0,0 +1,10 @@
+
+
+
+const GOOGLE_KEY ="AIzaSyBRloYp1iQhupSqVmsU65bmfQ5Eopfh8JE";
+
+
+
+export {
+    GOOGLE_KEY,
+};
