63349f0642c7c8fad9fed2f9d64bb41c9128ffba
Nouraiz <nouraiz_imran@hotmail.com>
2018-11-10 21:23:14 -0500

    Added bootstrap

--- polytalk/src/App.js
+++ polytalk/src/App.js
@@ -1,73 +1,79 @@
-import React, { Component } from 'react';
-import './App.css';
-import { GOOGLE_KEY } from './keys.js';
+import React, { Component } from "react";
+import "./App.css";
+import { GOOGLE_KEY } from "./keys.js";
 
-function ZipSearchField(props){
-  return(
-    <div> 
-    <label>ZipCode:</label>
-    <input onChange={props.changeHandler} type="text"  placeholder="10304" className="text-center"/>
+function ZipSearchField(props) {
+  return (
+    <div>
+      <label
+        style={{ fontWeight: "bold", fontSize: 20 }}
+        className="badge badge-pill m-2 badge-primary"
+      >
+        ZipCode:
+      </label>
+      <input
+        onChange={props.changeHandler}
+        type="text"
+        placeholder="10304"
+        className="text-center"
+      />
     </div>
-  )}
-
+  );
+}
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      officials : []
-    }
+      officials: []
+    };
   }
 
-
   zipCodeChanged(event) {
-    if(event.target.value.length !== 5){
-      this.setState({officials: []});
-      return(
+    if (event.target.value.length !== 5) {
+      this.setState({ officials: [] });
+      return (
         <div>
-        <p>No Result</p>
+          <p>No Result</p>
         </div>
-      )
+      );
     }
-    const url = "https://www.googleapis.com/civicinfo/v2/representatives?address="+ event.target.value +"&key=" + GOOGLE_KEY
+    const url =
+      "https://www.googleapis.com/civicinfo/v2/representatives?address=" +
+      event.target.value +
+      "&key=" +
+      GOOGLE_KEY;
 
-    fetch(url,{
+    fetch(url, {
       mode: "cors", // no-cors, cors, *same-origin
       cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
       credentials: "same-origin", // include, *same-origin, omit
       headers: {
-          "Content-Type": "application/json; charset=utf-8",
-          // "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Type": "application/json; charset=utf-8"
+        // "Content-Type": "application/x-www-form-urlencoded",
       },
       redirect: "follow", // manual, *follow, error
       referrer: "no-referrer" // no-referrer, *client
     })
-    .then( (response) => {
-      if(response.status === 200)
-        return(response.json())
-      else
-        console.log("There was an error")
-    })
-    .then( (body) => {
-      console.log(body)
-      this.setState({officials: body})
-      console.log(this.officials)
-
-
-    })
-    
+      .then(response => {
+        if (response.status === 200) return response.json();
+        else console.log("There was an error");
+      })
+      .then(body => {
+        console.log(body);
+        this.setState({ officials: body });
+        console.log(this.officials);
+      });
   }
 
   render() {
     return (
-      <div className = "App"> 
-      <h1>PoliTalk: Search Representatives</h1> 
-      <ZipSearchField changeHandler={(e) => this.zipCodeChanged(e)}/>
+      <div className="App">
+        <h1>PoliTalk: Search Representatives</h1>
+        <ZipSearchField changeHandler={e => this.zipCodeChanged(e)} />
       </div>
-      
-    ); 
-  };
+    );
+  }
 }
-
 
 export default App;
